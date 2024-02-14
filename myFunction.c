24b9877fc67f6edc468ee3df074e42bc30a305dc
GalKatzav <galoosh1591@gmail.com>
2024-02-14 16:43:19 +0200

    firstCommit

---
+++ myFunction.c
@@ -0,0 +1,68 @@
+#include "myFunction.h"
+
+char *getInputFromUser()
+{
+    char ch;
+    int size = 1;
+    int index = 0;
+
+    char *str = (char *)malloc(size * sizeof(char));
+
+    while ((ch = getchar()) != '\n')
+    {
+        *(str + index) = ch;
+        size++;
+        index++;
+        str = (char *)realloc(str, size * sizeof(char));
+    }
+    *(str + index) = '\0';
+
+    return str;
+}
+void blue()
+{
+
+    printf("\033[0;34m");
+}
+void reset()
+{
+
+    printf("\033[0m");
+}
+char **splitArgument(char *str)
+{
+    char *subStr;
+    int size = 2;
+    int index = 0;
+    subStr = strtok(str, " ");
+
+    char **arguments = (char **)malloc(size * sizeof(char *));
+
+    *(arguments + index) = subStr;
+
+    while ((subStr = strtok(NULL, " ")) != NULL)
+    {
+        size++;
+        index++;
+        *(arguments + index) = subStr;
+        arguments = (char **)realloc(arguments, size * sizeof(char *));
+    }
+    *(arguments + (index + 1)) = NULL;
+    return arguments;
+}
+
+void getLocation()
+{
+    char location[BUFF_SIZE];
+
+    if (getcwd(location, BUFF_SIZE) == NULL)
+    {
+        puts("Error");
+    }
+    else
+    {
+        blue();
+        puts(location);
+        reset();
+    }
+}
---
+++ myShell.h
@@ -0,0 +1,7 @@
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void welcome();
